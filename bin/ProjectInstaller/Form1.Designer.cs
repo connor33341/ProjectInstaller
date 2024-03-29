d73a6037e2227e4a0f3529c9034bb82086831015
Connor <107011324+connor33341@users.noreply.github.com>
2024-03-28 18:21:15 -0700

    v1.1 PREBUILD
    final version is 1.5

--- bin/ProjectInstaller/Form1.Designer.cs
+++ bin/ProjectInstaller/Form1.Designer.cs
@@ -28,12 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            splitContainer1 = new SplitContainer();
+            progressBar1 = new ProgressBar();
+            linkLabel1 = new LinkLabel();
+            label1 = new Label();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            label2 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            tabPage2 = new TabPage();
+            label3 = new Label();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.BackColor = SystemColors.ActiveCaption;
+            splitContainer1.Panel1.BackgroundImage = Properties.Resources.OIG4;
+            splitContainer1.Panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            splitContainer1.Panel1.Controls.Add(progressBar1);
+            splitContainer1.Panel1.Controls.Add(linkLabel1);
+            splitContainer1.Panel1.Controls.Add(label1);
+            splitContainer1.Panel1.Paint += splitContainer1_Panel1_Paint;
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(tabControl1);
+            splitContainer1.Size = new Size(800, 450);
+            splitContainer1.SplitterDistance = 266;
+            splitContainer1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(14, 379);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(238, 23);
+            progressBar1.TabIndex = 2;
+            progressBar1.Click += progressBar1_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(0, 435);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(31, 15);
+            linkLabel1.TabIndex = 1;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "v.1.5";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlText;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(240, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Project Installer";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(3, 3);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(527, 447);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(button2);
+            tabPage1.Controls.Add(textBox2);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(519, 419);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "General";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(106, 27);
+            label2.Name = "label2";
+            label2.Size = new Size(256, 30);
+            label2.TabIndex = 2;
+            label2.Text = "Git Repository (User/Repo)";
+            label2.Click += label2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(425, 60);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Verify";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(21, 60);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(383, 23);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "connor33341/projectinstaller";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(519, 419);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Advanced";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(106, 95);
+            label3.Name = "label3";
+            label3.Size = new Size(205, 30);
+            label3.TabIndex = 3;
+            label3.Text = "Installation Directory";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(425, 128);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Select";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(21, 128);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(383, 23);
+            textBox2.TabIndex = 4;
+            textBox2.Text = "C:\\Program Files\\{REPO NAME}\\";
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(800, 450);
+            Controls.Add(splitContainer1);
+            Name = "Form1";
+            Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private SplitContainer splitContainer1;
+        private Label label1;
+        private ProgressBar progressBar1;
+        private LinkLabel linkLabel1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button2;
+        private TextBox textBox2;
+        private Label label3;
+        private FolderBrowserDialog folderBrowserDialog1;
     }
 }
--- bin/ProjectInstaller/Form1.cs
+++ bin/ProjectInstaller/Form1.cs
@@ -1,3 +1,11 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Windows.Forms;
+
 namespace ProjectInstaller
 {
     public partial class Form1 : Form
@@ -5,6 +13,127 @@ namespace ProjectInstaller
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] parts = Path.GetFileNameWithoutExtension(Application.ExecutablePath).Split('-');
+        }
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string directory = "dir";
+            string mainExePath = Path.Combine(directory, "main.exe");
+            string repoPath = Path.Combine(directory, "repo");
+            string exeDirectory = Path.Combine(directory, "exe");
+
+            // Check if main.exe exists
+            if (!File.Exists(mainExePath))
+            {
+                // Create the directory if it doesn't exist
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                // Download and extract the repository
+                DownloadAndExtractRepository("user", "repo", repoPath);
+
+                // Move main.exe to the exe directory
+                MoveMainExe(repoPath, exeDirectory);
+            }
+            else
+            {
+                MessageBox.Show("main.exe already exists.");
+            }
+
+            // Run main.exe with --msg hi flags
+            RunMainExeWithFlags(exeDirectory);
+        }
+
+        private void DownloadAndExtractRepository(string user, string repo, string destinationPath)
+        {
+            string repoUrl = $"https://github.com/{user}/{repo}/archive/main.zip";
+
+            using (var client = new WebClient())
+            {
+                MessageBox.Show($"Downloading {user}/{repo}...");
+                client.DownloadFile(repoUrl, "repo.zip");
+            }
+
+            MessageBox.Show("Extracting repository...");
+            ZipFile.ExtractToDirectory("repo.zip", destinationPath);
+            File.Delete("repo.zip");
+        }
+
+        private void MoveMainExe(string sourceDirectory, string destinationDirectory)
+        {
+            string[] exeFiles = Directory.GetFiles(sourceDirectory, "main.exe", SearchOption.AllDirectories);
+
+            if (exeFiles.Length > 0)
+            {
+                MessageBox.Show("Moving main.exe to exe directory...");
+                Directory.CreateDirectory(destinationDirectory);
+                File.Move(exeFiles[0], Path.Combine(destinationDirectory, "main.exe"));
+                MessageBox.Show("main.exe moved successfully.");
+            }
+            else
+            {
+                MessageBox.Show("main.exe not found in the repository.");
+            }
+        }
+
+        private void RunMainExeWithFlags(string exeDirectory)
+        {
+            string exePath = Path.Combine(exeDirectory, "main.exe");
+
+            if (File.Exists(exePath))
+            {
+                MessageBox.Show("Running main.exe with --msg hi flags...");
+                Process.Start(exePath, "--msg hi");
+            }
+            else
+            {
+                MessageBox.Show("main.exe not found in the exe directory.");
+            }
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
