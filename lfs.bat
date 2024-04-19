713f8220da5486067c0fd5a9c953abc634c4bc56
Connor <107011324+connor33341@users.noreply.github.com>
2024-04-19 16:46:39 -0700

    Update

---
+++ lfs.bat
@@ -0,0 +1,10 @@
+@echo off
+git lfs track "*.dll"
+git lfs track "*.bin"
+git lfs track "*.exe"
+git lfs track "*.pdb"
+git lfs track "*.cache"
+git lfs track "*.vsidx"
+git lfs track "*.exe"
+echo Set LargeFileStorage
+pause
---
+++ test.bat
@@ -0,0 +1,2 @@
+echo %USERPROFILE%\.nuget\packages\ilmerge\%ILMERGE_VERSION%\tools\net452
+pause
